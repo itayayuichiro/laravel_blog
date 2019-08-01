da37a7e2abdaa68c74c14fa76f111d5465ad3765
Yuichiro Itaya <mr.itaya.magic@gmail.com>
2019-08-01 10:25:13 +0900

    feature: 画像投稿機能の実装

---
+++ resources/views/images/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="min_title">
+        <div class="container">
+            <h1 class="inline">
+                <? if (!empty($_GET['keyword'])){ ?>
+                「<?= $_GET['keyword'] ?>」
+                <? } ?>
+                TOPページ
+                <span><img class="inline" src="{{ asset('/img/report.png') }}"></span>
+            </h1>
+        </div>
+    </div>
+    <div class="container">
+        <br>
+        <div class="report_list">
+            @foreach ($contents as $content)
+                <div class="report">
+                    <div class="title">
+                        <h3>
+                            <a href="{{route('contents.show',$content->id)}}">
+                                {{ $content->title }}
+                            </a>
+                        </h3>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        {{ $contents->links() }}
+    </div>
+@endsection
